e74f07e01c21e6926fab7c2a75762d3d75c668b5
yu <yuchiu2002@hotmail.com>
2017-10-24 14:51:35 -0400

    completed

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const config = require('./config/config')
 const routes = require('./routes/')
+const fileUpload = require('express-fileupload');
 
 const app = express()
-routes(app)
-
 app
     .set("views", __dirname + "/views")
     .set("view engine", "hjs")
     .use(bodyParser.json())
     .use(cors())
+    .use(fileUpload());
+    
+routes(app)
 
 app.listen(config.port)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,24 @@
+const byteSize = require('byte-size')
+
 module.exports = (app) => {
-    app.get('/',
-            function (req, res) {
-                res.render("index")
+    app.get('/', function (req, res) {
+            res.render("index")
+        }),
+
+        app.post('/upload', function (req, res) {
+            try {
+                if (!req.files.uploadedFile) {
+                    res.status(400).send('No files were uploaded.');
+                } else {
+                    let uploadedFile = req.files.uploadedFile
+                    res.send({
+                        'name': uploadedFile.name,
+                        "mimetype": uploadedFile.mimetype,
+                        'size': byteSize(uploadedFile.data.length)
+                    })
+                }
+            } catch (err) {
+                res.send('error occured')
             }
-        )
+        })
 }
